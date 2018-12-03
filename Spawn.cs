ea285c18fe7533f16ce1f780e481dbab0217faa0
Javier Serra Campos <javier_serra_2@hotmail.es>
2018-12-03 10:58:05 +0100

    Update and rename spawn.cs to Spawn.cs

--- Spawn.cs
+++ Spawn.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class spawn : MonoBehaviour {
+public class Spawn : MonoBehaviour {
     private float lastSpawnTime;
     public float spawnInterval;
     public GameObject[] duckArray;
